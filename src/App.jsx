a6f10de0a7a585c6dfd95055e07121703b46afd2
anuj123-45 <anuj1358.be20@chikara.edu.in>
2024-10-22 18:42:20 +0530

    implement the routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,24 @@
 import React from 'react'
 import Navbar from './components/navbar'
-
+import Movies from './components/Movies'
+import WatchList from './components/WatchList'
+import { BrowserRouter, Routes , Route} from 'react-router-dom'
 
 function App() {
 
 
   return (
     <>
-    <Navbar/>
+      <BrowserRouter>
+
+        <Navbar />
+
+        <Routes>
+        <Route path='/' element={<Movies />} />
+        <Route path='/watchlist' element={<WatchList />} />
+        </Routes>
+
+      </BrowserRouter>
     </>
   )
 }
--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import Logo from '../assets/Movie-logo.jpg'
+import {Link} from 'react-router-dom'
 
 const navbar = () => {
   return (
     <div className='flex border items-center space-x-8 font-bold pl-3 py-3'>
       <img className='w-[50px]' src={Logo} alt="" />
-      <a href="/" className='text-blue-400 text-2xl'>Home</a>
-      <a href="/watchlist" className='text-blue-400 text-2xl'>Watchlist</a>
+      <Link to='/' className='text-blue-400 text-2xl'>Home</Link>
+      <Link to='/watchlist' className='text-blue-400 text-2xl'>Watchlist</Link>
     </div>
   )
 }
